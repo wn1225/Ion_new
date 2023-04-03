bd5a4a230c9beaa7425e3e81ac44ee78263b1a43
Clement Essien <clem.essien@gmail.com>
2023-04-02 21:33:14 -0500

    refactored divide_pdb.py  to remove accept  parameters from the command line

--- divide_pdb.py
+++ divide_pdb.py
@@ -1,3 +1,4 @@
+import argparse
 from Bio.PDB import Select, PDBIO
 from Bio.PDB.PDBParser import PDBParser
 
@@ -12,22 +13,32 @@ class ChainSelect(Select):
             return 0
 
 
-if __name__ == '__main__':
-    a = ''
-    chains = list(a)
-    with open('./data/data_list.txt','r') as fp:
-        for line in fp:
+def main():    
+    parser = argparse.ArgumentParser(description='supply the old and new directory to update the downloaded pdbs for a specific ion i.e. ZN, CA, CO3')
+    parser.add_argument('-input', dest='input', type=str, help='Specify the location of file that contain the pdb chains i.e. data_list.txt for the specific ion of interest', required=True)
+    parser.add_argument('-output', dest='output', type=str, help='Specify the location of pdb list i.e. data_list.txt file for the ion of interest', required=True)
+
+    args = parser.parse_args()
+
+    input = args.input
+    output = args.output
+    
+    with open(input,'r') as fp:
+        lines = fp.read().split('\n')
+        for line in lines:
             line = line.strip().split()
             name = line[0][:4].upper()
             chain = line[0][5]
-    # chains = ['A','G','H','I','J','K','L']
             p = PDBParser(PERMISSIVE=1)
-            pdb_file = './data/pdb_update/{}.pdb'.format(name)
+            pdb_file = output+'{}.pdb'.format(name)
             structure = p.get_structure(pdb_file, pdb_file)
 
             # for chain in chains:
-            pdb_chain_file = './data/model_one_pdb/{}_{}.pdb'.format(name,chain)
+            pdb_chain_file = output+'{}_{}.pdb'.format(name,chain)
             io_w_no_h = PDBIO()
             io_w_no_h.set_structure(structure)
             io_w_no_h.save('{}'.format(pdb_chain_file), ChainSelect(chain))
             print(pdb_chain_file)
+            
+if __name__ == '__main__':
+    main()
