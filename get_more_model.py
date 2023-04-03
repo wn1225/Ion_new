c832ff5e2fe6b7029c12689dc8d76f1a617fc182
Clement Essien <clem.essien@gmail.com>
2023-04-02 21:58:34 -0500

    refactored get_more_model.py

--- get_more_model.py
+++ get_more_model.py
@@ -1,21 +1,34 @@
 import os
 import glob
+import argparse
 '''
 Search out pdb files with multiple mods
 '''
+def main():
+    parser = argparse.ArgumentParser(description='supply the old and new directory to update the downloaded pdbs for a specific ion i.e. ZN, CA, CO3')
+    parser.add_argument('-path', dest='path', type=str, help='Specify the directory to search', required=True)
+    parser.add_argument('-output', dest='-output', type=str, help='Specify the output for the pdb files with multiple mods', required=True)
+    args = parser.parse_args()
 
-fw = open('./data/more_model_total1.txt', 'w')
-path = '.\data\one_pdb\*'
-for file_abs in glob.glob(path):
-    # print(file_abs)
-    # list.append(i)
-    max = 0
-    with open(file_abs,'r')as f:
-        for line in f:
-            row = line.strip().split()
-            if len(row) == 2 and row[0] == 'MODEL':
-                if max < int(row[1]):
-                    max = int(row[1])
-    if max != 0:
-         fw.write(file_abs[-10:-4] + ':' + str(max) + '\n')
+    path = args.path
+    output = args.output
+
+    fw = open(output, 'w')
+    path = path
+    for file_abs in glob.glob(path):
+        # print(file_abs)
+        # list.append(i)
+        max = 0
+        with open(file_abs,'r')as f:
+            for line in f:
+                row = line.strip().split()
+                if len(row) == 2 and row[0] == 'MODEL':
+                    if max < int(row[1]):
+                        max = int(row[1])
+        if max != 0:
+            fw.write(file_abs[-10:-4] + ':' + str(max) + '\n')
+    print("done generating more models!")
+if __name__ == "__main__":
+    main() 
+    
 
